02a6aabcd6eefe496b21457f2ed81e332354b426
AnastasiyaDziaruhina <stacy.deryugina@gmail.com>
2023-03-13 20:41:30 +0100

    Initial Commit

---
+++ NewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewApp
+//
+//  Created by Stacy on 13/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
