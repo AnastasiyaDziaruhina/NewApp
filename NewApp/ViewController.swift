2c86e31066844f816cde7916f2748a6bf84f5077
AnastasiyaDziaruhina <stacy.deryugina@gmail.com>
2023-03-18 16:46:51 +0100

    Main.storyboard design changed

--- NewApp/ViewController.swift
+++ NewApp/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.layer.bounds.width / 2
+        redLight.alpha = 0.5
+        yellowLight.layer.cornerRadius = yellowLight.layer.bounds.width / 2
+        yellowLight.alpha = 0.5
+        greenLight.layer.cornerRadius = greenLight.layer.bounds.width / 2
+        greenLight.alpha = 0.5
     }
 
-
+    @IBAction func startBtnPressed() {
+        
+    }
+    
 }
 
