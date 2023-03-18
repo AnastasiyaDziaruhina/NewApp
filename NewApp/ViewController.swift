83dd31428d4c2e232b5eca105cbf19984f9b0f1a
AnastasiyaDziaruhina <stacy.deryugina@gmail.com>
2023-03-18 18:22:23 +0100

    TrafficLight swith logic added

--- NewApp/ViewController.swift
+++ NewApp/ViewController.swift
@@ -8,16 +8,16 @@
 import UIKit
 
 final class ViewController: UIViewController {
-
+    
     @IBOutlet weak var redLight: UIView!
     @IBOutlet weak var yellowLight: UIView!
     @IBOutlet weak var greenLight: UIView!
+    @IBOutlet weak var buttonLabel: UIButton!
+    
+    private var trafficLightStarted = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    override func viewDidLayoutSubviews() {
         redLight.layer.cornerRadius = redLight.layer.bounds.width / 2
         redLight.alpha = 0.5
         yellowLight.layer.cornerRadius = yellowLight.layer.bounds.width / 2
@@ -25,10 +25,24 @@ final class ViewController: UIViewController {
         greenLight.layer.cornerRadius = greenLight.layer.bounds.width / 2
         greenLight.alpha = 0.5
     }
-
-    @IBAction func startBtnPressed() {
-        
-    }
     
+    @IBAction func startBtnPressed() {
+        if (!self.trafficLightStarted) {
+            buttonLabel.setTitle("Next", for: UIControl.State.normal)
+            self.trafficLightStarted = true
+            redLight.alpha = 1
+        } else {
+            if redLight.alpha == 1 {
+                redLight.alpha = 0.5
+                yellowLight.alpha = 1
+            } else if yellowLight.alpha == 1 {
+                yellowLight.alpha = 0.5
+                greenLight.alpha = 1
+            } else if greenLight.alpha == 1 {
+                greenLight.alpha = 0.5
+                redLight.alpha = 1
+            }
+        }
+    }
 }
 
